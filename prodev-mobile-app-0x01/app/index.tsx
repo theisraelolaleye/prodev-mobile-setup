fe7caeaae9f2fd9d8f82ac6ff4585acda33bda5c
theisraelolaleye <theisraelolaleye@gmail.com>
2025-10-28 09:39:54 +0100

    corrected

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,5 +1,20 @@
 import { Text, View, StyleSheet } from "react-native";
 
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text>Entry Screen - Awesome</Text>
+      <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+      <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+      <Text style={styles.smallText}>ALX is awesome</Text>
+    </View>
+  );
+}
+
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#90caf9",
@@ -25,16 +40,3 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
 });
-
-export default function Index() {
-  return (
-    <View
-      style={styles.container}
-    >
-      <Text>Entry Screen - Awesome</Text>
-      <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-      <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-      <Text style={styles.smallText}>ALX is awesome</Text>
-    </View>
-  );
-}
